3ba103907538346caf1ba749f73c33a46565e9cf
th-herve <126357296+th-herve@users.noreply.github.com>
2026-04-14 09:51:53 +0200

    Add footer year update

--- components/section/site-footer.tsx
+++ components/section/site-footer.tsx
@@ -6,9 +6,10 @@ interface Props {
 }
 
 const Footer = ({ className }: Props) => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className={cn("my-20 text-xs text-muted-foreground/60", className)}>
-      <TypographyP>@ 2025 th-herve</TypographyP>
+      <TypographyP>@ {currentYear} th-herve</TypographyP>
     </footer>
   );
 };
